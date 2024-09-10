1d597ff0344fd344508dd7ef04fee7943d933dc9
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2024-09-11 02:14:28 +0700

    update code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
 
-import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='flex justify-center items-center w-screen h-0 text-xs select-none'>
+    <div className='flex justify-center items-center w-screen h-0 text-[10px] pb-4 md:pb-0 lg:text-xs select-none'>
         <p className='text-white font-bold'>&copy; CS125 Nuttawoot Chawna SNRU | CodeCamp Academy 01</p>
     </div>
   )
