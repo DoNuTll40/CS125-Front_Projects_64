bdb19b6e7264be5245948996179a30a053c868f8
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2024-09-12 12:55:03 +0700

    update axios

--- src/configs/axios-path.js
+++ src/configs/axios-path.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // const ipAddress = window.location.hostname;
 
 // axios.defaults.baseURL = `http://${ipAddress}:8000`;
-axios.defaults.baseURL = `https://cs125-personal-projects-64.onrender.com/`;
+// axios.defaults.baseURL = `https://cs125-personal-projects-64.onrender.com/`;
 axios.defaults.baseURL = `https://cs-125-personal-projects-64-git-main-donuts-projects.vercel.app/`;
 
 export default axios;
